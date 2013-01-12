c7a060e92c12f7a5737b724d00dc824e14012760
Vishal Jain <vkjrul3z@gmail.com>
2013-01-12 15:54:50 -0500

    Initial commit(Vishal)
    Checking to see if things are working

--- src/com/example/gifting/MainActivity.java
+++ src/com/example/gifting/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	//blah blah test
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
