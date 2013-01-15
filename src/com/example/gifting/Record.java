6657c79b9356c236faac1b31b0b527afcb30bd1a
Saad <sbakht@eden.rutgers.edu>
2013-01-14 19:51:18 -0500

    Added alert when user doesn't have camera

--- src/com/example/gifting/Record.java
+++ src/com/example/gifting/Record.java
@@ -1,6 +1,8 @@
 package com.example.gifting;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -10,17 +12,17 @@ import android.view.Menu;
 public class Record extends Activity {
 
 	private static final int ACTION_TAKE_VIDEO = 3;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_record);
-		PackageManager pm=this.getPackageManager();
-		if(!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-			//create an alert dialog
-			
-		}else{
-			
+		PackageManager pm = this.getPackageManager();
+		if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+			// create an alert dialog
+			alertbox("Error", "You Do Not Have A Camera");
+		} else {
+
 		}
 	}
 
@@ -30,10 +32,23 @@ public class Record extends Activity {
 		getMenuInflater().inflate(R.menu.activity_record, menu);
 		return true;
 	}
-	
+
 	private void dispatchTakeVideoIntent() {
-	    Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-	    startActivityForResult(takeVideoIntent, ACTION_TAKE_VIDEO);
+		Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+		startActivityForResult(takeVideoIntent, ACTION_TAKE_VIDEO);
 	}
 
+	protected void alertbox(String title, String mymessage) {
+		new AlertDialog.Builder(this)
+				.setMessage(mymessage)
+				.setTitle(title)
+				.setCancelable(true)
+				.setNeutralButton(android.R.string.cancel,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								finish();
+							}
+						}).show();
+	}
 }
