83186a3ef00d3af64fb85d7619eaea913daced95
Vishal Jain <vkjrul3z@gmail.com>
2013-01-22 14:11:20 -0500

    Bringing up to date

--- src/com/example/gifting/Record.java
+++ src/com/example/gifting/Record.java
@@ -1,6 +1,15 @@
 package com.example.gifting;
 
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -10,7 +19,9 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.View;
@@ -63,6 +74,7 @@ public class Record extends Activity implements OnClickListener{
 
 	private void dispatchTakeVideoIntent() {
 		Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+		takeVideoIntent.putExtra("android.intent.extra.durationLimit", 5000);
 		startActivityForResult(takeVideoIntent, ACTION_TAKE_VIDEO);
 	}
 
@@ -113,4 +125,85 @@ public class Record extends Activity implements OnClickListener{
 		mImageView.setVisibility(View.INVISIBLE);
 	}
 
+	private void doFileUpload(){
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        DataInputStream inStream = null;
+        String existingFileName = Environment.getExternalStorageDirectory().getAbsolutePath() + "/mypic.png";
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary =  "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1*1024*1024;
+        String responseFromServer = "";
+        String urlString = "http://pushbackstudios.com/directory/upload.php";
+        try
+        {
+         //------------------ CLIENT REQUEST
+        FileInputStream fileInputStream = new FileInputStream(new File(existingFileName) );
+         // open a URL connection to the Servlet
+         URL url = new URL(urlString);
+         // Open a HTTP connection to the URL
+         conn = (HttpURLConnection) url.openConnection();
+         // Allow Inputs
+         conn.setDoInput(true);
+         // Allow Outputs
+         conn.setDoOutput(true);
+         // Don't use a cached copy.
+         conn.setUseCaches(false);
+         // Use a post method.
+         conn.setRequestMethod("POST");
+         conn.setRequestProperty("Connection", "Keep-Alive");
+         conn.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
+         dos = new DataOutputStream( conn.getOutputStream() );
+         dos.writeBytes(twoHyphens + boundary + lineEnd);
+         dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + existingFileName + "\"" + lineEnd);
+         dos.writeBytes(lineEnd);
+         // create a buffer of maximum size
+         bytesAvailable = fileInputStream.available();
+         bufferSize = Math.min(bytesAvailable, maxBufferSize);
+         buffer = new byte[bufferSize];
+         // read file and write it into form...
+         bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+         while (bytesRead > 0)
+         {
+          dos.write(buffer, 0, bufferSize);
+          bytesAvailable = fileInputStream.available();
+          bufferSize = Math.min(bytesAvailable, maxBufferSize);
+          bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+         }
+         // send multipart form data necesssary after file data...
+         dos.writeBytes(lineEnd);
+         dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+         // close streams
+         Log.e("Debug","File is written");
+         fileInputStream.close();
+         dos.flush();
+         dos.close();
+        }
+        catch (MalformedURLException ex)
+        {
+             Log.e("Debug", "error: " + ex.getMessage(), ex);
+        }
+        catch (IOException ioe)
+        {
+             Log.e("Debug", "error: " + ioe.getMessage(), ioe);
+        }
+        //------------------ read the SERVER RESPONSE
+        try {
+              inStream = new DataInputStream ( conn.getInputStream() );
+              String str;
+
+              while (( str = inStream.readLine()) != null)
+              {
+                   Log.e("Debug","Server Response "+str);
+              }
+              inStream.close();
+
+        }
+        catch (IOException ioex){
+             Log.e("Debug", "error: " + ioex.getMessage(), ioex);
+        }
+      }
 }
