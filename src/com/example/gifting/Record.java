5e1bab2bff415cde17874e896510517541bb9629
Vishal Jain <vkjrul3z@gmail.com>
2013-01-23 21:53:58 -0500

    Fixed making new directory
    Added external storage permission

--- src/com/example/gifting/Record.java
+++ src/com/example/gifting/Record.java
@@ -1,6 +1,5 @@
 package com.example.gifting;
 
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -36,7 +35,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
-public class Record extends Activity implements OnClickListener{
+public class Record extends Activity implements OnClickListener {
 
 	private static final int ACTION_TAKE_VIDEO = 3;
 
@@ -50,23 +49,23 @@ public class Record extends Activity implements OnClickListener{
 	private Bitmap mImageBitmap;
 	boolean mExternalStorageAvailable = false;
 	boolean mExternalStorageWriteable = false;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_record);
-		setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); 
-		//d=(Display)
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		// d=(Display)
 		PackageManager pm = this.getPackageManager();
 		if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
 			// create an alert dialog
 			alertbox("Error", "You Do Not Have A Camera");
-			
-		}else{
-			b1=(Button)findViewById(R.id.button1);
+
+		} else {
+			b1 = (Button) findViewById(R.id.button1);
 			b1.setOnClickListener(this);
-			//dispatchTakeVideoIntent();
-			
+			// dispatchTakeVideoIntent();
+
 			mImageView = (ImageView) findViewById(R.id.imageView1);
 			mVideoView = (VideoView) findViewById(R.id.videoView1);
 			mImageBitmap = null;
@@ -81,23 +80,202 @@ public class Record extends Activity implements OnClickListener{
 		return true;
 	}
 
-	private void dispatchTakeVideoIntent() {
-		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		Date date = new Date();
-		Uri fileUri = null;
-		fileUri.parse(dateFormat.format(date).toString());
-		System.out.println(dateFormat.format(date));
-		Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-		takeVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 5000);
-		//takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);  // set the image file name
+	
 
-		Uri uriSavedImage=Uri.fromFile(new File(Environment.getExternalStorageDirectory().getPath()+"/Redex/flashCroppededdr.mp4"));
-		takeVideoIntent.putExtra("output", uriSavedImage);
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.button1:
+			// checks if external storage is mounted properly
+			String state = Environment.getExternalStorageState();
+			if (Environment.MEDIA_MOUNTED.equals(state)) {
+				// We can read and write the media
+				mExternalStorageAvailable = mExternalStorageWriteable = true;
+				createDirIfNotExists(Environment.getExternalStorageDirectory()
+						.getPath() + "/Redex");
+				dispatchTakeVideoIntent();
+			} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+				// We can only read the media
+				mExternalStorageAvailable = true;
+				mExternalStorageWriteable = false;
+				alertbox("Read Only Error",
+						"External Storage is mounted Read-Only. Recommend Restart");
+			} else {
+				// Something else is wrong. It may be one of many other states,
+				// but all we need
+				// to know is we can neither read nor write
+				mExternalStorageAvailable = mExternalStorageWriteable = false;
+				alertbox("Read/Write Error",
+						"External Storage is does not have proper permissions. Recommend Restart");
+			}
 
-		//startActivityForResult(takeVideoIntent, ACTION_TAKE_VIDEO);
-		startActivityForResult(takeVideoIntent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
+			// basically the same as before but sends an empty string for the
+			// address and sends the string containing all previous searches
+			// dispatchTakeVideoIntent();
+			// this.setRequestedOrientation(d.getRotation());
+			break;
+		}
 	}
 
+	private void dispatchTakeVideoIntent() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+		Date date = new Date();
+		Uri fileUri = null;
+		String currentTime=dateFormat.format(date).toString();
+		fileUri.parse(dateFormat.format(date).toString());
+		//System.out.println(dateFormat.format(date));
+		Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+		takeVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 5000);
+		// takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set
+		// the image file name
+
+		Uri uriSavedImage = Uri.fromFile(new File(Environment
+				.getExternalStorageDirectory().getPath()
+				+ "/Redex/"+currentTime+".mp4"));
+		takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+
+		// startActivityForResult(takeVideoIntent, ACTION_TAKE_VIDEO);
+		startActivityForResult(takeVideoIntent,
+				CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+		// if (resultCode == RESULT_OK) {
+		// Image captured and saved to fileUri specified in the Intent
+		// Toast.makeText(this, "Image saved to:\n" +
+		// data.getData(), Toast.LENGTH_LONG).show();
+		// } else if (resultCode == RESULT_CANCELED) {
+		// User cancelled the image capture
+		// } else {
+		// Image capture failed, advise user
+		// }
+		// }
+
+		if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
+			if (resultCode == RESULT_OK) {
+				// Video captured and saved to fileUri specified in the Intent
+				Toast.makeText(this, "Video saved to:\n" + data.getData(),
+						Toast.LENGTH_LONG).show();
+			} else if (resultCode == RESULT_CANCELED) {
+				// User cancelled the video capture
+			} else {
+				// Video capture failed, advise user
+			}
+		}
+
+		// switch (requestCode) {
+		// case ACTION_TAKE_VIDEO: {
+		// if (resultCode == RESULT_OK) {
+		// handleCameraVideo(data);
+		// }
+		// break;
+		// } // ACTION_TAKE_VIDEO
+		// } // switch
+	}
+
+	private void handleCameraVideo(Intent intent) {
+		mVideoUri = intent.getData();
+		mVideoView.setVideoURI(mVideoUri);
+		mImageBitmap = null;
+		mVideoView.setVisibility(View.VISIBLE);
+		mImageView.setVisibility(View.INVISIBLE);
+	}
+
+	public static boolean createDirIfNotExists(String path) {
+		boolean ret = true;
+
+		File folder = new File(Environment.getExternalStorageDirectory()+"/Redex");
+		
+		if (!folder.exists()) {
+			//folder.mkdirs();
+			if (!folder.mkdirs()) {
+				Log.e("TravellerLog :: ", "Problem creating Image folder");
+				ret = false;
+			}
+		}
+		return ret;
+	}
+
+	private void doFileUpload() {
+		HttpURLConnection conn = null;
+		DataOutputStream dos = null;
+		DataInputStream inStream = null;
+		String existingFileName = Environment.getExternalStorageDirectory()
+				.getAbsolutePath() + "/mypic.png";
+		String lineEnd = "\r\n";
+		String twoHyphens = "--";
+		String boundary = "*****";
+		int bytesRead, bytesAvailable, bufferSize;
+		byte[] buffer;
+		int maxBufferSize = 1 * 1024 * 1024;
+		String responseFromServer = "";
+		String urlString = "http://pushbackstudios.com/directory/upload.php";
+		try {
+			// ------------------ CLIENT REQUEST
+			FileInputStream fileInputStream = new FileInputStream(new File(
+					existingFileName));
+			// open a URL connection to the Servlet
+			URL url = new URL(urlString);
+			// Open a HTTP connection to the URL
+			conn = (HttpURLConnection) url.openConnection();
+			// Allow Inputs
+			conn.setDoInput(true);
+			// Allow Outputs
+			conn.setDoOutput(true);
+			// Don't use a cached copy.
+			conn.setUseCaches(false);
+			// Use a post method.
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Connection", "Keep-Alive");
+			conn.setRequestProperty("Content-Type",
+					"multipart/form-data;boundary=" + boundary);
+			dos = new DataOutputStream(conn.getOutputStream());
+			dos.writeBytes(twoHyphens + boundary + lineEnd);
+			dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\""
+					+ existingFileName + "\"" + lineEnd);
+			dos.writeBytes(lineEnd);
+			// create a buffer of maximum size
+			bytesAvailable = fileInputStream.available();
+			bufferSize = Math.min(bytesAvailable, maxBufferSize);
+			buffer = new byte[bufferSize];
+			// read file and write it into form...
+			bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+			while (bytesRead > 0) {
+				dos.write(buffer, 0, bufferSize);
+				bytesAvailable = fileInputStream.available();
+				bufferSize = Math.min(bytesAvailable, maxBufferSize);
+				bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+			}
+			// send multipart form data necesssary after file data...
+			dos.writeBytes(lineEnd);
+			dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+			// close streams
+			Log.e("Debug", "File is written");
+			fileInputStream.close();
+			dos.flush();
+			dos.close();
+		} catch (MalformedURLException ex) {
+			Log.e("Debug", "error: " + ex.getMessage(), ex);
+		} catch (IOException ioe) {
+			Log.e("Debug", "error: " + ioe.getMessage(), ioe);
+		}
+		// ------------------ read the SERVER RESPONSE
+		try {
+			inStream = new DataInputStream(conn.getInputStream());
+			String str;
+
+			while ((str = inStream.readLine()) != null) {
+				Log.e("Debug", "Server Response " + str);
+			}
+			inStream.close();
+
+		} catch (IOException ioex) {
+			Log.e("Debug", "error: " + ioex.getMessage(), ioex);
+		}
+	}
 
 	protected void alertbox(String title, String mymessage) {
 		new AlertDialog.Builder(this)
@@ -112,175 +290,4 @@ public class Record extends Activity implements OnClickListener{
 							}
 						}).show();
 	}
-
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		switch (v.getId()) {
-		case R.id.button1:
-			//checks if external storage is mounted properly
-			String state = Environment.getExternalStorageState();
-			if (Environment.MEDIA_MOUNTED.equals(state)) {
-			    // We can read and write the media
-			    mExternalStorageAvailable = mExternalStorageWriteable = true;
-			    createDirIfNotExists(Environment.getExternalStorageDirectory().getPath()+"Redex/");
-			    dispatchTakeVideoIntent();
-			} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-			    // We can only read the media
-			    mExternalStorageAvailable = true;
-			    mExternalStorageWriteable = false;
-			    alertbox("Read Only Error", "External Storage is mounted Read-Only. Recommend Restart");
-			} else {
-			    // Something else is wrong. It may be one of many other states, but all we need
-			    //  to know is we can neither read nor write
-			    mExternalStorageAvailable = mExternalStorageWriteable = false;
-			    alertbox("Read/Write Error", "External Storage is does not have proper permissions. Recommend Restart");
-			}
-			
-			//basically the same as before but sends an empty string for the address and sends the string containing all previous searches
-			//dispatchTakeVideoIntent();
-			//this.setRequestedOrientation(d.getRotation());
-			break;
-		}
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-	        if (resultCode == RESULT_OK) {
-	            // Image captured and saved to fileUri specified in the Intent
-	            Toast.makeText(this, "Image saved to:\n" +
-	                     data.getData(), Toast.LENGTH_LONG).show();
-	        } else if (resultCode == RESULT_CANCELED) {
-	            // User cancelled the image capture
-	        } else {
-	            // Image capture failed, advise user
-	        }
-	    }
-
-	    if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
-	        if (resultCode == RESULT_OK) {
-	            // Video captured and saved to fileUri specified in the Intent
-	            Toast.makeText(this, "Video saved to:\n" +
-	                     data.getData(), Toast.LENGTH_LONG).show();
-	        } else if (resultCode == RESULT_CANCELED) {
-	            // User cancelled the video capture
-	        } else {
-	            // Video capture failed, advise user
-	        }
-	    }
-	    
-		//switch (requestCode) {
-		//case ACTION_TAKE_VIDEO: {
-			//if (resultCode == RESULT_OK) {
-				//handleCameraVideo(data);
-			//}
-			//break;
-		//} // ACTION_TAKE_VIDEO
-		//} // switch
-	}
-	
-	private void handleCameraVideo(Intent intent) {
-		mVideoUri = intent.getData();
-		mVideoView.setVideoURI(mVideoUri);
-		mImageBitmap = null;
-		mVideoView.setVisibility(View.VISIBLE);
-		mImageView.setVisibility(View.INVISIBLE);
-	}
-
-	public static boolean createDirIfNotExists(String path) {
-	    boolean ret = true;
-
-	    File file = new File(Environment.getExternalStorageDirectory(), path);
-	    if (!file.exists()) {
-	        if (!file.mkdirs()) {
-	            Log.e("TravellerLog :: ", "Problem creating Image folder");
-	            ret = false;
-	        }
-	    }
-	    return ret;
-	}
-	
-	private void doFileUpload(){
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null;
-        DataInputStream inStream = null;
-        String existingFileName = Environment.getExternalStorageDirectory().getAbsolutePath() + "/mypic.png";
-        String lineEnd = "\r\n";
-        String twoHyphens = "--";
-        String boundary =  "*****";
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1*1024*1024;
-        String responseFromServer = "";
-        String urlString = "http://pushbackstudios.com/directory/upload.php";
-        try
-        {
-         //------------------ CLIENT REQUEST
-        FileInputStream fileInputStream = new FileInputStream(new File(existingFileName) );
-         // open a URL connection to the Servlet
-         URL url = new URL(urlString);
-         // Open a HTTP connection to the URL
-         conn = (HttpURLConnection) url.openConnection();
-         // Allow Inputs
-         conn.setDoInput(true);
-         // Allow Outputs
-         conn.setDoOutput(true);
-         // Don't use a cached copy.
-         conn.setUseCaches(false);
-         // Use a post method.
-         conn.setRequestMethod("POST");
-         conn.setRequestProperty("Connection", "Keep-Alive");
-         conn.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
-         dos = new DataOutputStream( conn.getOutputStream() );
-         dos.writeBytes(twoHyphens + boundary + lineEnd);
-         dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + existingFileName + "\"" + lineEnd);
-         dos.writeBytes(lineEnd);
-         // create a buffer of maximum size
-         bytesAvailable = fileInputStream.available();
-         bufferSize = Math.min(bytesAvailable, maxBufferSize);
-         buffer = new byte[bufferSize];
-         // read file and write it into form...
-         bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-         while (bytesRead > 0)
-         {
-          dos.write(buffer, 0, bufferSize);
-          bytesAvailable = fileInputStream.available();
-          bufferSize = Math.min(bytesAvailable, maxBufferSize);
-          bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-         }
-         // send multipart form data necesssary after file data...
-         dos.writeBytes(lineEnd);
-         dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-         // close streams
-         Log.e("Debug","File is written");
-         fileInputStream.close();
-         dos.flush();
-         dos.close();
-        }
-        catch (MalformedURLException ex)
-        {
-             Log.e("Debug", "error: " + ex.getMessage(), ex);
-        }
-        catch (IOException ioe)
-        {
-             Log.e("Debug", "error: " + ioe.getMessage(), ioe);
-        }
-        //------------------ read the SERVER RESPONSE
-        try {
-              inStream = new DataInputStream ( conn.getInputStream() );
-              String str;
-
-              while (( str = inStream.readLine()) != null)
-              {
-                   Log.e("Debug","Server Response "+str);
-              }
-              inStream.close();
-
-        }
-        catch (IOException ioex){
-             Log.e("Debug", "error: " + ioex.getMessage(), ioex);
-        }
-      }
-	
 }
